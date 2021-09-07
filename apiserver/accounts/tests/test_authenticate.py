38bef6b4ab7ad16f95673fe5c025a9ffd196b43f
hannal <kay@hannal.net>
2021-09-07 23:50:21 +0900

    인증 payload 기본 validation 구현

--- apiserver/accounts/tests/test_authenticate.py
+++ apiserver/accounts/tests/test_authenticate.py
@@ -15,3 +15,6 @@ class AuthenticateTest(TestCase):
     def test_auth_failure(self):
         res = self.get(self.base_url)
         self.assertEqual(res.status_code, 405)
+
+        res = self.post(self.base_url)
+        self.assertEqual(res.status_code, 400)
--- apiserver/accounts/views.py
+++ apiserver/accounts/views.py
@@ -5,4 +5,9 @@ def authenticate(request):
     if request.method.lower() != 'post':
         return HttpResponse(status=405)
 
+    username = request.POST.get('username') or ''
+    password = request.POST.get('password') or ''
+
+    if not username or not password:
+        return HttpResponse('invalid payload', status=400)
     return HttpResponse('hello world')
