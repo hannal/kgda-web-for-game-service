a39ebc2c20d06dcd37c99c5391481ebf1f34897e
hannal <kay@hannal.net>
2021-09-08 11:06:25 +0900

    docker로 띄운 pg에 연결하도록 설정.

--- apiserver/apiserver/settings/local.py
+++ apiserver/apiserver/settings/local.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webdb',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',  # docker
+        'PORT': '5432',  # docker
+    },
+}
