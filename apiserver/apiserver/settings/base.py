ff37514a3ffa320101b52a4b60590a8f44bb84f1
hannal <kay@hannal.net>
2021-09-08 10:12:27 +0900

    django settings 정리. local settings 추가.

--- apiserver/apiserver/settings/base.py
+++ apiserver/apiserver/settings/base.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-)f6%3nnjco_-poym^r*+!#dbukzw@8g!jhan7#xnbdtcn*04se
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
